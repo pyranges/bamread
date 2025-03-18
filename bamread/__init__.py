c2a832631013a43db15c82048d35e3cf7113cf0e
endrebak.ada <endrebak85@gmail.com>
2025-03-18 16:20:41 +0100

    Use importlib not pkg_resources

--- bamread/__init__.py
+++ bamread/__init__.py
@@ -1,6 +1,15 @@
-import pkg_resources
+try:
+    # For Python 3.8+
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    # For older Python versions, install the backport:
+    # pip install importlib_metadata
+    from importlib_metadata import version, PackageNotFoundError
 
-__version__ = pkg_resources.get_distribution("bamread").version
+try:
+    __version__ = version("bamread")
+except PackageNotFoundError:
+    __version__ = "unknown"
 
 from bamread.read import read_bam, read_bam_full
 
